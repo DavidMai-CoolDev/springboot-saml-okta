9eabf6ed90cd98c089d5a95b72a22a0be8fbb84d
MaiDuy <maiduy191992@gmail.com>
2020-07-01 08:14:19 +0700

    initial commit

---
+++ src/main/java/com/example/selflearnign/demo/springboot/okta/saml/TestController.java
@@ -0,0 +1,21 @@
+package com.example.selflearnign.demo.springboot.okta.saml;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class TestController {
+    @GetMapping("/user")
+    public Authentication getUserInfor(Principal principal){
+        System.out.println("The response file is: " + SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+
+
+        return (Authentication) principal;
+
+
+    }
+}
